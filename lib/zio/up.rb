4f7e8ec94c5acbf52371094f04c1eaebe5922b96
Andrea Campolonghi <acampolonghi@gmail.com>
2015-08-30 13:27:16 +0200

    Use Celluloid notifcations to avoid mixing out console logging

--- lib/zio/up.rb
+++ lib/zio/up.rb
@@ -6,28 +6,29 @@ module Zio
   class UpLogger
     include Celluloid
     include Celluloid::Notifications
-    attr_accessor :out, :last
+    attr_accessor :out, :paths
 
     def initialize
-      subscribe 'up_last_path', :last_path
+      subscribe 'up_paths', :set_paths
       subscribe 'up_capture_complete', :new_data
       @out = []
+      @paths = [1]
     end
 
     def new_data(_topic, data)
-      puts data
       @out << data
     end
 
-    def last_path(_topic, data)
-      puts data
-      @last = data
+    def set_paths(_topic, data)
+      @paths.concat(data)
     end
 
     def print
-      current_path = 'path'
-      while last != current_path do
-        next if out.empty?
+      while paths.size > 0 do
+        if out.empty?
+          sleep 2
+          next
+        end
         data = out.shift
         path, stdout, stderr, status = *data
         dirname = path.split('/').last
@@ -38,7 +39,8 @@ module Zio
         else
           STDERR.puts stderr.color(:red)
         end
-        current_path = path
+        paths.delete(path)
+        paths.delete(1) if paths.size == 1 && paths[0] == 1
       end
     end
   end
@@ -55,25 +57,25 @@ module Zio
     end
 
     def up
-      publish 'up_last_path', _paths.last
+      publish 'up_paths', _paths
       _paths.each do |path|
         #Celluloid::Future.new { _pull(path) }
         # future.value
         #self.class.pool(size: 8, args: dir).future._pull(path)
-        #_pull(path)
-        self.future._pull(path)
+        # pull(path)
+        self.class.new(dir).async.pull(path)
       end
     end
 
-    def _pull(path)
+    def pull(path)
       stdout, stderr, status = Open3.capture3("cd #{path} && git stash && git checkout master && git pull --rebase")
-      Celluloid::Notifications.publish 'up_capture_complete', [path, stdout, stderr, status]
+      publish 'up_capture_complete', [path, stdout, stderr, status]
     end
 
     def _paths
       @_paths ||= begin
         Dir.foreach(dir).map do |path|
-          next if path == '.' || path == '..'
+          next if path == '.' || path == '..' || !File.exists?(File.expand_path(path, dir) + '/.git')
           ab_path = File.expand_path(path, dir)
           File.directory?(ab_path) ? ab_path : nil
         end.compact
